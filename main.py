10765428ab7a0da25ba24c4e23021a21f1073b77
Carla <carlaty07@gmail.com>
2024-12-17 19:20:50 +0800

    learning class and OOP

--- main.py
+++ main.py
@@ -16,9 +16,26 @@ Functionality:
 --Can view summary report (e.g total monthly spending)
 
 """
-name = input("Enter your name: ")
-age = input("Enter your age: ")
+#League Champions
+# health, attack points and method to "Flight"
+# defending and attacking sided
+# monsters with abilities
 
-with open("user_info.txt", "w") as f:
-    f.write(name)
-    f.write(age)
+class Monster:
+    
+    def __init__(self, name, hp, attack):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+    
+    def fight(self, target):
+        target.hp -= self.attack
+        print(f"{self.name} attacked {target.name} for {self.attack} damage!")
+        print(f"{target.name} has {target.hp} HP left")
+        
+    
+
+Morgana = Monster("Morgana", 100, 25)
+Lux = Monster("Lux", 50, 35)
+Morgana.fight(Lux)
+Lux.fight(Morgana)
